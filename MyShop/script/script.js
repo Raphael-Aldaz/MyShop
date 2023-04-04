dc8207892222f9734ee230438ea485195f64b75e
Raphael-Aldaz <raphael.aldaz@gmail.com>
2023-04-04 19:36:42 +0200

    linking event beetween change and value

--- MyShop/script/script.js
+++ MyShop/script/script.js
@@ -1,8 +1,9 @@
 import { data } from "../data/data.js";
 const elementTab = document.querySelector('.article__item')
 const catButton = document.querySelectorAll('.list__cat__item')
-
+const basketSection = document.querySelector('.basket__item');
 let a ;
+let basket = [];
 
 const displayData = (arr) => {
    elementTab.innerHTML=""
@@ -24,10 +25,7 @@ const displayData = (arr) => {
                 elementInput.classList = 'quantity__articles'
 
                 elementInput.addEventListener('change',handleChange)
-                elementInput.addEventListener('click',  ()=> {
-                    a = e
-                })
-              
+
                 elementTr.appendChild(elementTd)
                 elementTd.appendChild(elementInput)
             }
@@ -41,7 +39,11 @@ const displayData = (arr) => {
 
     })
 }
-
+const diplayBasket = (arr) =>{
+    console.log(arr)
+    const test = arr.entries.id
+    basketSection.appendChild(test)
+}
 const getId = (e) => {
     const newData = [];
     data.map((article)=> {
@@ -53,18 +55,27 @@ const getId = (e) => {
     })
     displayData(newData);
     }
-const handleChange =(e) => {
- const basket = []
- basket.push(a)
- if(a  < 0){
-    console.log("vide")
- } else {
-    console.log(basket)
- }
- 
 
+const handleChange = (event) => {
+    
+    const id = event.target.parentNode.parentNode.getAttribute('id');
+    const value = parseInt(event.target.value);
+    data.map((item) => {
+        
+    if (item.id == id) {
+        
+        item.Quantity = value;
+        basket.push(item)
+    }
+    });
+    const uniqueBasket = new Set(basket);
+    const sortedbasket = Array.from(uniqueBasket).sort();
+
+    diplayBasket(uniqueBasket);
 }
+let test = handleChange
 
+console.log(test)
 for(const sortButton of catButton){
     sortButton.addEventListener('click', getId)
 }
